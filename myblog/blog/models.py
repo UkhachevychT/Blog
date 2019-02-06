39f9f5f7002bbb1c77839a18e34e6568711756ca
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2019-02-06 16:17:13 +0200

    make slugs unique

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.template.defaultfilters import slugify
 
+import itertools
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
@@ -14,7 +16,14 @@ class Post(models.Model):
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
-            self.slug = slugify(self.title)
+
+            self.slug = orig = slugify(self.title)
+
+            for x in itertools.count(1):
+                if not Post.objects.filter(slug=self.slug).exists():
+                    break
+                self.slug = '%s-%d' % (orig, x)
+
         self.modified = timezone.now()
         return super(Post, self).save(*args, **kwargs)
 
