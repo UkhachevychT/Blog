6aeb26c3faef30d321fc565861d56e1096011354
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2019-02-06 16:06:12 +0200

    create Post model and detail page

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    img = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+    slug = models.SlugField(unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+            self.slug = slugify(self.title)
+        self.modified = timezone.now()
+        return super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from. import models
 
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = models.Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'posts': posts,
+    })
+
+def detail(request, slug):
+    post = get_object_or_404(models.Post, slug=slug)
+
+    return render(request, 'blog/post.html', {
+        'post': post,
+    })
